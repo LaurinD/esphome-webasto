8311a016e3a9cb1a61c45282c2171cf780659c0c
Laurin Döpfner <laurin.doepfner@gmail.com>
2026-02-27 22:29:07 +0100

    Update __init__.py

--- components/webasto/__init__.py
+++ components/webasto/__init__.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
+DEPENDENCIES = ["uart"]
+
+webasto_ns = cg.esphome_ns.namespace("webasto")
+Webasto = webasto_ns.class_("Webasto", cg.Component, uart.UARTDevice)
+
+CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(Webasto),
+    }
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
