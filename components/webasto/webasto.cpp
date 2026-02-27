cca42335f2500abec93f99682c06f27c5b3f5ae7
Laurin Döpfner <laurin.doepfner@gmail.com>
2026-02-27 23:38:47 +0100

    Update webasto.cpp

--- components/webasto/webasto.cpp
+++ components/webasto/webasto.cpp
@@ -1,5 +1,2 @@
-#include "webasto.h"  // dort ist die Klasse Webasto drin
-
-using namespace esphome;       // optional
-using namespace esphome::uart; // für UARTComponent
-using namespace esphome::webasto; // <--- das fehlt
+#include "webasto.h"
+using namespace esphome::webasto;
